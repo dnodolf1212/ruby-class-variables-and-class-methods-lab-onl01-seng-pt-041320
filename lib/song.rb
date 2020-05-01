fd449fb9385070b4a3667f04da8ba0b121ef0dbc
Dwight Nodolf <dnodolf@gmail.com>
2020-05-01 20:06:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   def self.genre_count
     hash = {}
     @@genres.each do |genre|
-      if hash.key? == true 
+      if hash.key?(genre) == true 
         hash[genre] += 1  
       else
         hash[genre] = 1
