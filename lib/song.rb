6ea44c2fd8ee8bf0e76146d502f53d6263924a5a
Dwight Nodolf <dnodolf@gmail.com>
2020-05-01 16:52:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def initialize(name, artist, genre)
     @@count += 1
     @name = name
-    @artist = @@artists << artist
+    @artist = artist #@@artists << artist
     @genre = genre 
   end
   
