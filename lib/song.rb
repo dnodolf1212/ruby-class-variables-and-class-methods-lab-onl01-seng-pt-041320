d81f789b8f9787a75edf477801d2d436bf6d0344
Dwight Nodolf <dnodolf@gmail.com>
2020-05-01 16:11:59 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre 
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres << genre  #contol for duplicates (if/ else, match? look at school domain)
+  end
+  
+  def artists
+    @@artists << artist  #control for duplicates
+  
+end
