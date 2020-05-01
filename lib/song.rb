ed86b1fd3346c9cb6f200f71ab7c7b8caec2d190
Dwight Nodolf <dnodolf@gmail.com>
2020-05-01 20:21:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
     hash = {}
     @@genres.each do |genre, x|
       if hash.key?(genre) == true 
-        hash[genre]     
+        hash[genre] += 1      
       else
         hash[genre] = 1
    end
